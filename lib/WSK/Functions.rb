94fe739214bba7867c825240ad15ef138f5b3510
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2010-10-14 21:30:31 +0000

    * Added operations on functions (applyMap, substract, divide)
    git-svn-id: https://waveswissknife.svn.sourceforge.net/svnroot/waveswissknife@18 2a4dbb28-0437-4655-84f5-72fa8c1bc6cd

--- lib/WSK/Functions.rb
+++ lib/WSK/Functions.rb
@@ -159,6 +159,138 @@ module WSK
         end
       end
 
+      # Apply a mapping function to this function.
+      #
+      # Parameters:
+      # * *iMapFunction* (_Function_): The mapping function
+      def applyMapFunction(iMapFunction)
+        case @Function[:FunctionType]
+        when FCTTYPE_PIECEWISE_LINEAR
+          case iMapFunction.functionData[:FunctionType]
+          when FCTTYPE_PIECEWISE_LINEAR
+            # Both functions are piecewise linear
+            # Algorithm:
+            # * For each segment of our function:
+            # ** We look at the segments from the map function.
+            # ** For each found segment:
+            # *** We find at which abscisses this segment will change values
+            # *** We change the sub-segment between those abscisses
+            lPoints = @Function[:Points]
+            lMapPoints = iMapFunction.functionData[:Points]
+            lNewPoints = []
+            lIdxSegment = 0
+            while (lIdxSegment < lPoints.size-1)
+              lBeginX = lPoints[lIdxSegment][0]
+              lBeginY = lPoints[lIdxSegment][1]
+              lEndX = lPoints[lIdxSegment+1][0]
+              lEndY = lPoints[lIdxSegment+1][1]
+              # The direction in which we are going to look for the map segments
+              lIncMapSegment = nil
+              if (lEndY >= lBeginY)
+                lIncMapSegment = true
+              else
+                lIncMapSegment = false
+              end
+              # Find the map function's segment containing the beginning of our segment
+              lIdxMapSegment = 0
+              while (lBeginY >= lMapPoints[lIdxMapSegment+1][0])
+                ++lIdxMapSegment
+              end
+              # Compute the new value of our segment beginning
+              lNewBeginY = lMapPoints[lIdxMapSegment][1] + ((lMapPoints[lIdxMapSegment+1][1]-lMapPoints[lIdxMapSegment][1])*(lBeginY-lMapPoints[lIdxMapSegment][0]))/(lMapPoints[lIdxMapSegment+1][0]-lMapPoints[lIdxMapSegment][0])
+              lNewPoints << [ lBeginX, lNewBeginY ]
+              # Get the next map segments unless we reach our segment's end
+              # !!! Find the next map segment according to the direction
+              if (lIncMapSegment)
+                while (lEndY > lMapPoints[lIdxMapSegment+1][0])
+                  # We have a new map segment to consider in our segment
+                  # Find the absciss at which our Y coordinates get the value lMapPoints[lIdxMapSegment+1][0]
+                  lNewSegmentX = lBeginX + ((lEndX-lBeginX)*(lMapPoints[lIdxMapSegment+1][0] - lBeginY))/(lEndY-lBeginY)
+                  lNewPoints << [ lNewSegmentX, lMapPoints[lIdxMapSegment+1][1] ]
+                  ++lIdxMapSegment
+                end
+                # Our segment ends before next map segment
+              else
+                while (lEndY <= lMapPoints[lIdxMapSegment][0])
+                  # We have a new map segment to consider in our segment
+                  # Find the absciss at which our Y coordinates get the value lMapPoints[lIdxMapSegment][0]
+                  lNewSegmentX = lBeginX + ((lEndX-lBeginX)*(lMapPoints[lIdxMapSegment][0] - lBeginY))/(lEndY-lBeginY)
+                  lNewPoints << [ lNewSegmentX, lMapPoints[lIdxMapSegment][1] ]
+                  --lIdxMapSegment
+                end
+                # Our segment ends before previous map segment
+              end
+              # Write the segment end if it is the last one (otherwise it will be written by the next iteration)
+              if (lIdxSegment == lPoints.size-2)
+                lNewEndY = lMapPoints[lIdxMapSegment][1] + ((lMapPoints[lIdxMapSegment+1][1]-lMapPoints[lIdxMapSegment][1])*(lEndY-lMapPoints[lIdxMapSegment][0]))/(lMapPoints[lIdxMapSegment+1][0]-lMapPoints[lIdxMapSegment][0])
+                lNewPoints << [ lEndX, lNewEndY ]
+              end
+              ++lIdxSegment
+            end
+            # Replace with new points
+            @Function[:Points] = lNewPoints
+          else
+            logErr "Unknown function type: #{@Function[:FunctionType]}"
+          end
+        else
+          logErr "Unknown function type: #{@Function[:FunctionType]}"
+        end
+      end
+
+      # Substract a function to this function
+      #
+      # Parameters:
+      # * *iSubFunction* (_Function_): The function to substract
+      def substractFunction(iSubFunction)
+        case @Function[:FunctionType]
+        when FCTTYPE_PIECEWISE_LINEAR
+          case iSubFunction.functionData[:FunctionType]
+          when FCTTYPE_PIECEWISE_LINEAR
+            lNewPoints = []
+            unionXWithFunction_PiecewiseLinear(iSubFunction) do |iX, iY, iOtherY|
+              lNewPoints << [ iX, iY - iOtherY ]
+            end
+            # Replace with new points
+            @Function[:Points] = lNewPoints
+          else
+            logErr "Unknown function type: #{@Function[:FunctionType]}"
+          end
+        else
+          logErr "Unknown function type: #{@Function[:FunctionType]}"
+        end
+      end
+
+      # Divide this function by another function
+      #
+      # Parameters:
+      # * *iDivFunction* (_Function_): The function that divides
+      def divideByFunction(iDivFunction)
+        case @Function[:FunctionType]
+        when FCTTYPE_PIECEWISE_LINEAR
+          case iDivFunction.functionData[:FunctionType]
+          when FCTTYPE_PIECEWISE_LINEAR
+            lNewPoints = []
+            unionXWithFunction_PiecewiseLinear(iDivFunction) do |iX, iY, iOtherY|
+              lNewPoints << [ iX, iY / iOtherY ]
+            end
+            # Replace with new points
+            @Function[:Points] = lNewPoints
+          else
+            logErr "Unknown function type: #{@Function[:FunctionType]}"
+          end
+        else
+          logErr "Unknown function type: #{@Function[:FunctionType]}"
+        end
+      end
+
+      # Get the internal function data
+      #
+      # Return:
+      # * <em>map<Symbol,Object></em>: The internal function data
+      def functionData
+        return @Function
+      end
+
       private
 
       # Prepare the Function utils C library.
@@ -176,6 +308,42 @@ module WSK
         if (@VolumeUtils == nil)
           require 'WSK/VolumeUtils/VolumeUtils'
           @VolumeUtils = WSK::VolumeUtils::VolumeUtils.new
+        end
+      end
+
+      # Find abscisses of both functions and the corresponding Y values
+      #
+      # Parameters:
+      # * *iOtherFunction* (_Function_): The other function
+      # * *CodeBlock*: Code called for each point found:
+      # ** *iX* (_Integer_): The corresponding abscisse (can be nil if none)
+      # ** *iY* (_Float_): The corresponding Y value for this function (can be nil if none)
+      # ** *iOtherY* (_Float_): The corresponding Y value for the other function
+      def unionXWithFunction_PiecewiseLinear(iOtherFunction)
+        lPoints = @Function[:Points]
+        lOtherPoints = iOtherFunction.functionData[:Points]
+        # Get all the abscisses sorted
+        lXList = (lPoints.map { |iPoint| next iPoint[0] } + lOtherPoints.map { |iPoint| next iPoint[0] }).sort.uniq
+        # Read segments abscisse by abscisse
+        lIdxSegment = 0
+        lIdxOtherSegment = 0
+        lXList.each do |iX|
+          if (lPoints[lIdxSegment][0] == iX)
+            if (lOtherPoints[lIdxOtherSegment][0] == iX)
+              # If both functions have a point here, it's easy.
+              yield(iX, lPoints[lIdxSegment][1], lOtherPoints[lIdxOtherSegment][1])
+              ++lIdxOtherSegment
+            else
+              # Compute the Y value for the other function
+              yield(iX, lPoints[lIdxSegment][1], lOtherPoints[lIdxOtherSegment-1][1] + ((lOtherPoints[lIdxOtherSegment][1] - lOtherPoints[lIdxOtherSegment-1][1])*(iX - lOtherPoints[lIdxOtherSegment-1][0]))/(lOtherPoints[lIdxOtherSegment][0] - lOtherPoints[lIdxOtherSegment-1][0]))
+            end
+            ++lIdxSegment
+          else
+            # We have forcefully lOtherPoints[lIdxOtherSegment][0] == iX
+            # Compute the Y value for this function
+            yield(iX, lPoints[lIdxSegment-1][1] + ((lPoints[lIdxSegment][1] - lPoints[lIdxSegment-1][1])*(iX - lPoints[lIdxSegment-1][0]))/(lPoints[lIdxSegment][0] - lPoints[lIdxSegment-1][0]), lOtherPoints[lIdxOtherSegment][1])
+            ++lIdxOtherSegment
+          end
         end
       end
 
